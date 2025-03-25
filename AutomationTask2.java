a2806c3b632a7030a67ed3cd01abfe619de73b00
Jaykishan Varma <113700513+kishan-stack@users.noreply.github.com>
2025-03-26 00:13:22 +0530

    Update AutomationTask2.java

--- AutomationTask2.java
+++ AutomationTask2.java
@@ -1,65 +1,33 @@
-package org.example;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.ArrayList;
-import java.util.Set;
+import java.time.Duration;
 
-public class AutomationTask2 {
-    WebDriver driver;
-    public AutomationTask2(WebDriver d){
-        this.driver = d;
-    }
-
-
-    public void SignIn(){
-        driver.findElement(By.xpath("//input[@id='ap_email']")).sendKeys("stevewhitman11@gmail.com");
-        driver.findElement(By.xpath("//input[@id='continue']")).click();
-//        ap_password
-        driver.findElement(By.xpath("//input[@id='ap_password']")).sendKeys("Amazonpass");
-//        signInSubmit
-        driver.findElement(By.xpath("//input[@id='signInSubmit']")).click();
-
-
-    }
-    public void SearchFunctionality(String item){
-        driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys(item);
-        driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
-        JavascriptExecutor jsx = (JavascriptExecutor) driver;
-        jsx.executeScript("window.scroll(0,350)");
-    }
-
-    public void SelectProductAndAddToCart(){
-        driver.findElement(By.xpath("//span[text()='Video Camera Camcorder kimire Digital Camera Recorder Full HD 1080P 15FPS 24MP 3.0 Inch 270 Degree Rotation LCD 16X Digital Zoom Camcorder Camera with 2 Batteries(Black)']")).click();
-        Set<String> allWindows = driver.getWindowHandles();
-        System.out.println(allWindows);
-        ArrayList<String>windows = new ArrayList<>();
-        for (String window:allWindows){
-            windows.add(window);
-        }
-        driver.switchTo().window(windows.get(1));
-        String current = driver.getWindowHandle();
-        driver.findElement(By.xpath("//input[@id='add-to-cart-button']")).click();
-        driver.findElement(By.xpath("//a[@href='/cart?ref_=sw_gtc']")).click();
-
-        //
-//        Go to Cart
-    }
-
-
+public class EcomAutomation {
     public static void main(String[] args) {
-        WebDriver d = new ChromeDriver();
-        AutomationTask2 at2 = new AutomationTask2(d);
-        at2.driver.get("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fgp%2Fcart%2Fview.html%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
-        at2.driver.manage().window().maximize();
-        at2.SignIn();
-        at2.SearchFunctionality("camcorder");
-        at2.SelectProductAndAddToCart();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://amazon.in");
+        driver.manage().window().maximize();
+        WebElement input = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+        input.sendKeys("toys");
+        input.sendKeys(Keys.ENTER);
+        JavascriptExecutor jsx = (JavascriptExecutor)driver;
+        jsx.executeScript("window.scroll(0,250)");
 
+        WebDriverWait wt = new WebDriverWait(driver, Duration.ofSeconds(8));
+        WebElement item1 = wt.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='a-autoid-3-announce']")));
+        item1.click();
+        WebElement item2 = wt.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='a-autoid-4-announce']")));
+//        item2.click();
+        WebElement item3 = wt.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='a-autoid-5-announce']")));
+//        item3.click();
+        WebElement item4 = wt.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='a-autoid-6-announce']")));
+        item4.click();
+        WebElement cart = wt.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/gp/cart/view.html?ref_=nav_cart']")));
+        cart.click();
+//
+        System.out.println(driver.findElement(By.xpath("//div[@class='sc-badge-price-to-pay sc-apex-cart-price-to-pay']//span[@class='a-offscreen']")).getText());
     }
-
 }
